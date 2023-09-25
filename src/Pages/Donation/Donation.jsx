8ae98cad8038ad4e4d612e7a5de2f59ec1b6ab0c
mdmomenulislam1 <mdmomenulislam1@gmail.com>
2023-09-25 17:58:46 +0600

    Dynamically See more Button total and delete button Completed

--- src/Pages/Donation/Donation.jsx
+++ src/Pages/Donation/Donation.jsx
@@ -14,14 +14,19 @@ const Donation = () => {
 
     const [isShow, setIsShow] = useState(false);
 
-    const 
+    const [totalDonation, setTotalDonation] = useState(0)
 
 
 
     useEffect(() => {
-        const DonatedItem = JSON.parse(localStorage.getItem('Donated'))
+        const DonatedItem = JSON.parse(localStorage.getItem('Donated'));
+
         if (DonatedItem) {
-            setDonated(DonatedItem)
+            setDonated(DonatedItem);
+
+            const total = DonatedItem.reduce((preDonation, currentDonation) => preDonation + currentDonation.price, 0)
+
+            setTotalDonation(total);
         }
         else {
             setEmpty('No data Available')
@@ -39,7 +44,13 @@ const Donation = () => {
     return (
         <div>
 
-            {Donated.length > 0 && <button onClick={handleRemove} className="hover:bg-black my-20 btn mx-auto bg-purple-950 block text-white">Delete All Donations </button>}
+            {
+            Donated.length > 0 && 
+            <div className="flex flex-col lg:flex-row justify-between my-5 ">
+                <button onClick={handleRemove} className="hover:bg-black p-4 font-semibold rounded bg-purple-950 text-white">Delete All Donations </button>
+                <h1 className="text-4xl font-bold">Total Donation: ${totalDonation}</h1>
+            </div>
+            }
 
             <div className="">
                 {empty ? <p className="flex justify-center items-center py-20 text-4xl font-bold">No Data Found</p> : <div>
@@ -48,27 +59,27 @@ const Donation = () => {
 
                         {
                             isShow ? Donated.map(donation => (
-                                    <DonatedCard key={donation.id} donation={donation}></DonatedCard>
-                                ))
-                                :Donated.slice(0,4).map(donation =>(
+                                <DonatedCard key={donation.id} donation={donation}></DonatedCard>
+                            ))
+                                : Donated.slice(0, 4).map(donation => (
                                     <DonatedCard key={donation.id} donation={donation}></DonatedCard>
                                 ))
                         }
-                        
+
 
 
                     </div>
                     {
                         Donated.length > 4 && <button onClick={() => setIsShow(!isShow)} className="hover:bg-black my-20 btn mx-auto bg-purple-950 block text-white">
-                        {isShow?"Show Less":"Show More"}
-                    </button>
+                            {isShow ? "Show Less" : "Show More"}
+                        </button>
                     }
 
                 </div>}
 
             </div>
 
-            
+
         </div>
     );
 };
