90eccbb09d8832e1d81f8558e66cac392f81268d
mdmomenulislam1 <mdmomenulislam1@gmail.com>
2023-09-26 23:47:55 +0600

    Fake data related issues and deploy issues

--- src/Pages/ErrorPage/ErrorPage.jsx
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const ErrorPage = () => {
     return (
         <div className="h-screen flex justify-center items-center">
-            Error
+            <img src="https://i.ibb.co/YcVBYS0/image.png" alt="" />
         </div>
     );
 };
--- src/Pages/Statistics/Statistics.jsx
+++ src/Pages/Statistics/Statistics.jsx
@@ -30,3 +30,47 @@ const Statistics = () => {
 };
 
 export default Statistics;
+
+
+
+
+
+// PieChart.js
+import React from 'react';
+import { Pie } from 'react-chartjs-2';
+
+const PieChart = ({ data }) => {
+  return (
+    <div>
+      <h2>Pie Chart</h2>
+      <Pie data={data} />
+    </div>
+  );
+};
+
+export default PieChart;
+
+// MainComponent.js
+import React from 'react';
+import PieChart from './PieChart';
+
+const MainComponent = () => {
+  const chartData = {
+    labels: ['Data One', 'Data Two'],
+    datasets: [
+      {
+        data: [40, 60],
+        backgroundColor: ['#FC2525', '#05CBE1'],
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <h1>Main Component</h1>
+      <PieChart data={chartData} />
+    </div>
+  );
+};
+
+export default MainComponent;
